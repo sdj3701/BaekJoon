01b27d3c1c42c9724625cd8c3c86343aa5b8d718
신동주 <sdj3701@naver.com>
2025-04-08 18:39:42 +0900

    Feat : 2346

--- CodingTest/CodingTestExample/Test.cpp
+++ CodingTest/CodingTestExample/Test.cpp
@@ -14,7 +14,47 @@ using namespace std;
 
 void baekjoon()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int n, top, number;
+	deque<pair<int, int>> dq;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		dq.push_back(make_pair(i, num));
+	}
+
+	number = dq.front().second;
+	cout << dq.front().first << " ";
+
+	dq.pop_front(); // 터진거 제거
+
+	while (!dq.empty()) {
+		if (number > 0) { // 양
+			for (int i = 0; i < number - 1; i++) { // 터진거 빼고 다시 deque생성
+				dq.push_back(dq.front());
+				dq.pop_front();
+			}
+			number = dq.front().second;
+			cout << dq.front().first << " ";
+			dq.pop_front();
+		}
+		else { // 음
+			for (int i = 0; i > number + 1; i--) { // 반대로 deque생성
+				dq.push_front(dq.back());
+				dq.pop_back(); // -1 2 1 -> 1 -1 2 1 -> 1 -1 2 -> 2 1 -1 2 -> 2 1 -1
+			}
+			number = dq.back().second;
+			cout << dq.back().first << " ";
+			dq.pop_back();
+		}
+
+	}
 }
 
 
